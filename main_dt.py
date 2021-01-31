63f188f8da22bd37040b5660a154777ff7e6a823
sarahalyaat <47882708+sarahalyaat@users.noreply.github.com>
2021-01-31 23:41:23 +0700

    Edit some attributes for heart disease prediction; accuracy increased

--- main_dt.py
+++ main_dt.py
@@ -54,7 +54,9 @@ def DTpredict(data_target,predict_target):
 		feature_cols = ['glucose', 'BMI', 'Age','dbp']
 	elif (predict_target == 'Penyakit Jantung'):
 		data = data_heart
-		feature_cols = ['Age','sbp','chol','fbs','thalach','oldpeak','slope','cp']
+		feature_cols = ['Age','sbp','chol','fbs','cp']
+		#cp: chest pain type (1-typical angina; 2-atypical angina 3-non-anginal pain; 4-asymptomatic)
+		#fbs: fasting blood sugar > 120 mg/dl (1 = true; 0 = false) 
 
 	# split dataset in features and target variable
 	X = data[feature_cols] # Features -> independent variables
@@ -163,6 +165,30 @@ def cekData_search():
 		while ((valid != 0) and (valid != 1)):
 			valid = int(input("Pilihan tidak valid! Masukkan kembali pilihan Anda: "))
 	clear()
+	return()
+
+def cekData_update(filename):
+	#Inisiasi Variabel
+	valid = 0
+	#Fungsi
+	while (valid == 0):
+		clear()
+		print("\nUPDATE DATA KESEHATAN\n___________________________\nMasukkan Data: \n")
+		#Update Data (Sesuaikan dengan data apa saja yang akan diinput)
+		id_upd = input("No. ID: ")
+		name_upd = input("Nama Lengkap: ")
+		bulan_upd = int(today.strftime("%m"))
+		tahun_upd = int(today.strftime("%Y"))
+		angka_upd = input("Angka: ")
+		new_data = {'id': [ide_upd], 'nama':[name_upd], 'bulan':[bulan_upd], 'tahun':[tahun_upd], 'angka':[angka_upd]}
+		df_new_data = pd.DataFrame(new_data)
+		# data = data.append(new_upd, ignore_index=True)
+		df_new_data.to_csv(filename, mode='a', header=False, index=False)
+		print("\nData berhasil disimpan!\n")
+		#Pilihan
+		valid = int(input("Lanjutkan Update Data?   0. Ya 	1. Tidak\nPilihan: "))
+		while ((valid != 0) and (valid != 1)):
+			valid = int(input("Pilihan tidak valid! Masukkan kembali pilihan Anda: "))
 	return()
 
 def menu_cekGizi():
